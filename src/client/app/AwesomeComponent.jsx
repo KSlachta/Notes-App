70daa56cde2d8b92b425f29d62dbdcfb642c6ad4
Kyle-Slachta <Kyle.Slachta@ibm.com>
2017-06-07 15:57:39 -0400

    almost have custom list

--- src/client/app/AwesomeComponent.jsx
+++ src/client/app/AwesomeComponent.jsx
@@ -13,17 +13,33 @@ class AwesomeComponent extends React.Component {
 
   onClick = () => {
     let newListCount = this.state.listCount + 1;
-    let newWords = [...this.state.words, 'Hello World!']
+    let newWords = [...this.state.words, this.state.word]
     this.setState({
       listCount: newListCount,
-      words: newWords
+      words: newWords,
+      word: ''
     });
+  }
+
+  onTextChange = (e) => {
+      this.setState({word: e.target.value})
   }
 
   render() {
     return (
       <div>
-        <div><button onClick={this.onClick}>Click this!</button></div>
+        <div>
+          <form onSubmit={this.onClick}>
+              <input type='text'
+                  id='input'
+                  placeholder={'write text'}
+                  onChange={this.onTextChange}/>
+                <button
+                  onMouseDown={this.onClick}
+                  id='button'/>
+          </form>
+
+        </div>
         List : <span>{this.state.listCount}</span>
         {this.state.words.map(function(w) {
           return(<div>{w}</div>)
